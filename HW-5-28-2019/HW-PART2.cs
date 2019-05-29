8d6bed27ed2de30908cc1a1ab2140d262b393d27
xinyashan <50379822+xinyashan@users.noreply.github.com>
2019-05-29 08:50:48 -0500

    Add files via upload

---
+++ HW-5-28-2019/HW-PART2.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace HW2_XIN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] pokemart =
+{
+               "Pokeballs",
+               "Super potions",
+               "Berries",
+               "Escape rope",
+               "Great ball",
+               "Ultra ball"
+             };
+            int index = 0;
+            foreach (string item in pokemart)
+            {
+                Console.WriteLine(item);
+                Console.WriteLine(index);
+                Console.WriteLine(pokemart[index]);
+                index++;
+            }
+
+
+            Pokemart();
+            dialogue("Welcome to the pokemart! Today we have a sale going on!");
+            dialogue("If you buy 10 pokeballs then you 1 free special pokeball as a bonus!");
+
+            int result = equation(99, 100);
+            Console.WriteLine(result);
+        }
+
+
+
+
+
+
+        private static void Pokemart()
+        {
+            Console.WriteLine("BONUS: Buy 10 and get 1 pokeball for free!");
+
+
+        }
+        private static void dialogue(string words)
+        {
+
+        }
+
+
+        private static int equation(int num1, int num2)
+        {
+            int returnVariable = num1 + num2;
+            return returnVariable;
+        }
+
+    }
+}
+
+
+
+
+        }
+    }
+}
---
+++ HW-5-28-2019/HW-Xinya.cs
@@ -0,0 +1,110 @@
+﻿using System;
+
+namespace Homework_xinya
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("Hello! Welcome to the Poke'mart traveler! I see you're running low on items. You need more Pokeballs if you want to " +
+                "catch more Pokemon. Please browse through our category ");
+
+            Console.WriteLine("Which items would you like to buy?");
+
+            Console.WriteLine("Pick a choice: 1, Pokeball x99, 2, potions x99, 3, Repels x99");
+            string answer1 = Console.ReadLine();
+
+            switch (answer1)
+            {
+                case "1":
+                    Console.WriteLine("Thanks for buying the pokeballs! Please come again!");
+                    break;
+
+                case "2":
+                    Console.WriteLine("Thanks for buying the potions! Please come again!");
+                    break;
+
+                case "3":
+                    Console.WriteLine("Thanks for buying the repels! PLease come again!");
+                    break;
+
+                default:
+                    Console.WriteLine("Sorry we can't get that item for you. Would you like to browse through our catagory again?");
+                    break;
+            }
+
+            {
+                Console.WriteLine("Hi there! It seems we have more items in our shop");
+                Console.WriteLine("Which item would you like to buy from our special list?");
+
+                Console.WriteLine("Pick a choice: 1, Max potion x99, 2, Revival herbs x99, 3, Master ball x99");
+                string answer2 = Console.ReadLine();
+                switch (answer2)
+                {
+                    case "1":
+                        Console.WriteLine("Thanks for buying the max potions! Heal up your party members and continue your quest!");
+                        break;
+
+                    case "2":
+                        Console.WriteLine("Thanks for buying the revival herbs! Your Pokemons will thank you.");
+                        break;
+
+                    case "3":
+                        Console.WriteLine("Thanks for buying the master balls! You can now catch any Pokemon you want!");
+                        break;
+
+                    default:
+                        Console.WriteLine("Unfortunatly that is not available.");
+                        break;
+                }
+
+
+            }
+
+            {
+                Console.WriteLine("Our secret shop also gives customer a chance to adopt some rare Pokemon.");
+                Console.WriteLine("Which Pokemon do you want to obtain? 1, Shiny Charizard, 2, Shiny Blastoise, 3, Shiny Venusaur");
+
+                string answer3 = Console.ReadLine();
+                switch (answer3)
+                {
+                    case "1":
+                        Console.WriteLine("You chose CHARIZARD! It is level 50!");
+                        Console.WriteLine("[Moves: Fire Punch, fly, flamethrower, and Hyper beam]");
+                        Console.WriteLine("Item held: Sitrus berry"); 
+                        break;
+
+                    case "2":
+                        Console.WriteLine("You choose BLASTOISE! It is level 50!");
+                        Console.WriteLine("[Moves: Scald, ice beam, aqua jet, and dragon tail]");
+                        Console.WriteLine("Item held: Chocolate"); 
+                        break;
+
+                    case "3":
+                        Console.WriteLine("You chose VENESAUR! It is level 50!");
+                        Console.WriteLine("[Moves: Solar beam, Sunny day, Toxic, and Reflect]");
+                        Console.WriteLine("Item held: Cherry pie"); 
+                        break;
+
+                    default:
+                        Console.WriteLine("Sorry we can't get that Pokemon for you!");
+                        break;
+
+                }
+            }
+
+        }
+
+}
+
+}
+   
+
+
+
+
+
+
+
